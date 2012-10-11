217f99edd83b5ee242e4c84b0f3a77ca87f34ed9
matalvepu <kamrulhasan326@gmail.com>
2012-10-11 11:20:16 +0600

    fuck map

--- application/models/weatherDataTemp.php
+++ application/models/weatherDataTemp.php
@@ -4,8 +4,11 @@ class WeatherDataTemp extends CI_Model
 {
 	function avgMaxTempMonthYear($sid,$month,$year)
         {
-            $q = $this->db->query("SELECT AVG( maxtemp ) AS a FROM weatherdata WHERE sid = ? AND MONTH( wdate ) = ? AND YEAR( wdate      ) = ?",array($sid,$month,$year));
+            $q = $this->db->query("SELECT AVG( maxtemp ) AS a FROM weatherdata WHERE sid = ? AND MONTH(wdate) = ? AND YEAR(wdate) = ?",array($sid,$month,$year));
             return $q->row()->a;
+			
+			
+			
         }
 
         function avgMinTempMonthYear($sid,$month,$year)
